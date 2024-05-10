fb04e26e6b1a8eb171bcaefc2d07363714bff789
Rajendra Vechalapu <vrsanaidu@gmail.com>
2024-05-11 10:43:11 +1200

    further changes

---
+++ R-Learning/ImportYoutubeVideo.py
@@ -0,0 +1,47 @@
+from pytube import YouTube
+import os
+import cv2
+
+def download_video(url, output_path):
+    yt = YouTube(url)
+    stream = yt.streams.filter(file_extension='mp4').first()
+    stream.download(output_path=output_path)
+    return stream.default_filename
+
+def extract_frames(video_path, output_folder):
+    cap = cv2.VideoCapture(video_path)
+    frame_count = 0
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        frame_path = os.path.join(output_folder, f"frame_{frame_count}.jpg")
+        cv2.imwrite(frame_path, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])  # Ensure maximum quality
+        print(f"Frame {frame_count} saved")
+
+        frame_count += 1
+
+    cap.release()
+
+# Example usage
+url = "https://www.youtube.com/watch?v=WSorSPJogEA"  # Replace with your YouTube video URL
+output_folder = "D:/ApowerRECData/Import"  # Change this to your desired output folder
+
+# Delete all files within the output folder before processing
+for file in os.listdir(output_folder):
+    file_path = os.path.join(output_folder, file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception as e:
+        print(f"Failed to delete {file_path}. Reason: {e}")
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+video_filename = download_video(url, output_folder)
+video_path = os.path.join(output_folder, video_filename)
+print("Video Path:" + video_path)
+#extract_frames(video_path, output_folder)
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ApacheSpark
+python -m venv rajenv
+#pip freeze > requirements.txt
+.\rajenv\Scripts\activate
+
+$env:DATABASE_URL = "postgresql://rajpostgressql_user:QfEIQR6tHAfUWFSwIIUxhmlAd4ozL0lZ@dpg-cma94evqd2ns73bd3bmg-a.oregon-postgres.render.com/rajpostgressql"
+$env:FLASK_APP = "myapp"
+flask run --reload
+
+pip install -r requirements.txt
+
+--Uninstalls
+pip list
+pip freeze > unins ; pip uninstall -y -r unins ; del unins
